d4aee21805ec057e180e3b356a243053e05a88fb
Owen Lee <lee@Owen-Lee-Rogers.local>
2022-12-14 12:02:11 +0300

    done with number slicing

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -11,15 +11,18 @@ const CoinItem = ({ coin }) => {
       <td>{coin.market_cap_rank}</td>
       <td>
         <div className="flex items-center">
-          <img src={coin.image} alt={coin.id} />
-          <p>{coin.name}</p>
+          <img className="w-6 mr-2 rounded-full " src={coin.image} alt={coin.id} />
+          <p className="hidden sm:table-cell">{coin.name}</p>
         </div>
       </td>
       <td>{coin.symbol}</td>
       <td>{coin.current_price}</td>
-      <td>{coin.price_change_percentage_24h}</td>
-      <td>{coin.market_cap}</td>
-      <td>{coin.total_volume}</td>
+      <td>
+        {coin.price_change_percentage_24h > 0 ? (<p className="text-green-600">{coin.price_change_percentage_24h}</p>) : (<p className="text-red-600">{coin.price_change_percentage_24h}</p>)}
+        
+        </td>
+      <td className="w-[180px] hidden md:table-cell">{coin.total_volume}</td>
+      <td className="W-[180px] hidden sm:table-cell ">{coin.market_cap}</td>
       <td>
         <Sparklines data={coin.sparkline_in_7d.price}>
           <SparklinesLine color="teal" />
