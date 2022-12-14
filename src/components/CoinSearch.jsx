7d5afe7a96c85c102962a6d23fc0e7cea08b63a4
Owen Lee <lee@Owen-Lee-Rogers.local>
2022-12-14 11:34:54 +0300

    Still styling the Crypto table

---
+++ src/components/CoinSearch.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+import CoinItem from "./CoinItem";
+
+const CoinSearch = ({ coins }) => {
+  const [searchText, setSearchText] = useState("");
+
+  // console.log(coins)
+  return (
+    <div className="rounded-div my-4 ">
+      <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
+        <h1 className="text-2xl font-bold my-2 ">Search Crypto</h1>
+        <form>
+          <input onChange={(e) => setSearchText(e.target.value)} className="" type="text" placeholder="Search a coin" />
+        </form>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th>Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>24h Volume</th>
+            <th>Mkt</th>
+            <th>Last 7 Days</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {coins
+            .filter((value) => {
+              if (searchText === "") {
+                return value;
+              } else if (
+                value.name.toLowerCase().includes(searchText.toLowerCase())
+              ) {
+                return value;
+              }
+            })
+            .map((coin) => (
+              <CoinItem key={coin.id} coin={coin} />
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CoinSearch;
