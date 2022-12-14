16363f0b83330c4d09ac78e78179117104bcda12
Owen Lee <lee@Owen-Lee-Rogers.local>
2022-12-14 12:10:17 +0300

    done with the Frontend part of the Crypto project

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -11,18 +11,25 @@ const CoinItem = ({ coin }) => {
       <td>{coin.market_cap_rank}</td>
       <td>
         <div className="flex items-center">
-          <img className="w-6 mr-2 rounded-full " src={coin.image} alt={coin.id} />
+          <img
+            className="w-6 mr-2 rounded-full "
+            src={coin.image}
+            alt={coin.id}
+          />
           <p className="hidden sm:table-cell">{coin.name}</p>
         </div>
       </td>
-      <td>{coin.symbol}</td>
-      <td>{coin.current_price}</td>
+      <td>{coin.symbol.toUpperCase()}</td>
+      <td>${coin.current_price.toLocaleString()}</td>
       <td>
-        {coin.price_change_percentage_24h > 0 ? (<p className="text-green-600">{coin.price_change_percentage_24h}</p>) : (<p className="text-red-600">{coin.price_change_percentage_24h}</p>)}
-        
-        </td>
-      <td className="w-[180px] hidden md:table-cell">{coin.total_volume}</td>
-      <td className="W-[180px] hidden sm:table-cell ">{coin.market_cap}</td>
+        {coin.price_change_percentage_24h > 0 ? (
+          <p className="text-green-600">{coin.price_change_percentage_24h.toFixed(2)}%</p>
+        ) : (
+          <p className="text-red-600">{coin.price_change_percentage_24h.toFixed(2)}%</p>
+        )}
+      </td>
+      <td className="w-[180px] hidden md:table-cell">${coin.total_volume.toLocaleString()}</td>
+      <td className="W-[180px] hidden sm:table-cell ">${coin.market_cap.toLocaleString()}</td>
       <td>
         <Sparklines data={coin.sparkline_in_7d.price}>
           <SparklinesLine color="teal" />
