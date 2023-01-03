a9a0af4b8882a613fc131f2901b1a2abb7c77eaa
Owenzo7 <owenlee545@gmail.com>
2023-01-03 15:43:51 +0300

    added an env file

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCybwvktafO2cT1akCcOV6I-28kC1AtmVA",
+  authDomain: "cryptobase-c15e1.firebaseapp.com",
+  projectId: "cryptobase-c15e1",
+  storageBucket: "cryptobase-c15e1.appspot.com",
+  messagingSenderId: "509209535380",
+  appId: "1:509209535380:web:14d368361bebcb2320e581"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
