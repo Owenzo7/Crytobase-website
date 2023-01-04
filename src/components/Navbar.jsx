dad91b28b6ff288c4b8aa7a019c3b060f8b8f3f6
Owenzo7 <owenlee545@gmail.com>
2023-01-04 16:51:49 +0300

    formatted the code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,27 +32,27 @@ const Navbar = () => {
       <div className="hidden md:block">
         <ThemeToggle />
       </div>
-       
 
-       {user?.email ? (
+      {user?.email ? (
         <div>
-          <Link to="/account" className="p-4">Account</Link>
+          <Link to="/account" className="p-4">
+            Account
+          </Link>
           <button onClick={handleSignOut}>Sign out</button>
         </div>
-       ) : (
+      ) : (
         <div className="hidden md:block">
-        <Link to="/signin" className="p-4 hover:text-accent">
-          Sign In
-        </Link>
-        <Link
-          to="/signup"
-          className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
-        >
-          Sign Up
-        </Link>
-      </div>
-       )}
-
+          <Link to="/signin" className="p-4 hover:text-accent">
+            Sign In
+          </Link>
+          <Link
+            to="/signup"
+            className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
+          >
+            Sign Up
+          </Link>
+        </div>
+      )}
 
       {/* Menu Icon */}
       <div onClick={handleNav} className="block md:hidden cursor-pointer z-10">
